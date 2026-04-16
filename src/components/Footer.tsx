cc938485a08849b4844c2acf62336958df6bcb63
agsijw <m1x1m2x1789@gmail.com>
2026-04-16 11:40:11 +0300

    Update footer text for copyright notice

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="bg-gray-800 text-white py-8">
       <div className="container mx-auto px-4 text-center">
-        <p>جميع الحقوق محفوظة © 2024 - معاً لفهم التوحد</p>
+        <p>"لا يتبع هاذا الموقع اي جهة رسمية"</p>
       </div>
     </footer>
   );
